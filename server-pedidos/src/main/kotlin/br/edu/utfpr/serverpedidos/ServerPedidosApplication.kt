849ad2dd4a38dbe2087769f1d7125df880d860fb
João Guilherme Brasil Pichetti <joaopichetti@Joaos-MacBook-Pro.local>
2024-05-18 12:06:14 -0300

    Commit inicial

---
+++ server-pedidos/src/main/kotlin/br/edu/utfpr/serverpedidos/ServerPedidosApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.utfpr.serverpedidos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerPedidosApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerPedidosApplication>(*args)
+}
---
+++ server-pedidos/src/test/kotlin/br/edu/utfpr/serverpedidos/ServerPedidosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.utfpr.serverpedidos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerPedidosApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
